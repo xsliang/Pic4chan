ea7639585bce74ba4863069bd4c27260b26ff277
xsliang <xsliang17@hotmail.com>
2015-09-29 16:33:13 +0800

    first

---
+++ Pic4chan/FormMain.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Pic4chan
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pcbMS = new System.Windows.Forms.PictureBox();
+            this.OK = new System.Windows.Forms.Button();
+            this.url = new System.Windows.Forms.TextBox();
+            this.resultText = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbMS)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcbMS
+            // 
+            this.pcbMS.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pcbMS.Location = new System.Drawing.Point(764, 41);
+            this.pcbMS.Name = "pcbMS";
+            this.pcbMS.Size = new System.Drawing.Size(497, 561);
+            this.pcbMS.TabIndex = 0;
+            this.pcbMS.TabStop = false;
+            // 
+            // OK
+            // 
+            this.OK.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OK.Location = new System.Drawing.Point(268, 10);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(75, 23);
+            this.OK.TabIndex = 1;
+            this.OK.Text = "OK";
+            this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            // 
+            // url
+            // 
+            this.url.Location = new System.Drawing.Point(12, 14);
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(250, 19);
+            this.url.TabIndex = 2;
+            this.url.Text = "http://www.4chan.org/ck/";
+            // 
+            // resultText
+            // 
+            this.resultText.BackColor = System.Drawing.Color.Black;
+            this.resultText.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultText.ForeColor = System.Drawing.Color.Lime;
+            this.resultText.Location = new System.Drawing.Point(12, 41);
+            this.resultText.Multiline = true;
+            this.resultText.Name = "resultText";
+            this.resultText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.resultText.Size = new System.Drawing.Size(726, 561);
+            this.resultText.TabIndex = 3;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 986);
+            this.Controls.Add(this.resultText);
+            this.Controls.Add(this.url);
+            this.Controls.Add(this.OK);
+            this.Controls.Add(this.pcbMS);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pcbMS)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcbMS;
+        private System.Windows.Forms.Button OK;
+        private System.Windows.Forms.TextBox url;
+        private System.Windows.Forms.TextBox resultText;
+    }
+}
+
---
+++ Pic4chan/FormMain.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pic4chan
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void GetWebInfo(string url)
+        {
+            try
+            {
+                resultText.Text = string.Empty;
+                HttpWebRequest httpWebRequest = WebRequest.CreateHttp(url);
+                //httpWebRequest.BeginGetResponse(new AsyncCallback(CallBackMethod), httpWebRequest);
+                WebResponse webresponse = httpWebRequest.GetResponse();
+                StreamReader sr = new StreamReader(webresponse.GetResponseStream());
+                List<string> result = ProcessHtml(sr.ReadLine());
+                //resultText.Text = sr.ReadLine();
+                foreach (var item in result)
+                {
+                    GetPictrueFormNet(item);
+                    resultText.Text = resultText.Text + "\r\n" + item;
+                }
+
+                MessageBox.Show("Completed!");
+
+            }
+            catch (Exception ex)
+            {
+                resultText.Text = ex.Message;
+            }
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        private void GetPictrueFormNet(string item)
+        {
+            try
+            {
+                HttpWebRequest httpWebRequest = WebRequest.CreateHttp("http:" + item);
+                WebResponse webresponse = httpWebRequest.GetResponse();
+                //StreamReader sr = new StreamReader(webresponse.GetResponseStream());
+                Image image = Image.FromStream(webresponse.GetResponseStream());
+                image.Save("C:\\" + item.Split('/')[item.Split('/').Length - 1]);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        private List<string> ProcessHtml(string html)
+        {
+            List<string> result = new List<string>();
+
+            Regex regex = new Regex(@"<a class=""fileThumb""[^>]+>");
+            MatchCollection matchs = regex.Matches(html);
+            //result = matchs;
+            foreach (var item in matchs)
+            {
+                Regex regexPicture = new Regex(@"href=""//[^""]+""");
+                Match picName = regexPicture.Match(item.ToString());
+                string sPicName = picName.Value;
+                sPicName = sPicName.Replace("href=\"", string.Empty);
+                sPicName = sPicName.Replace("\"", string.Empty);
+                result.Add(sPicName.ToString());
+            }
+            return result;
+        }
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+            GetWebInfo(url.Text);
+        }
+    }
+}
