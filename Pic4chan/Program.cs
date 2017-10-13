610f51afaa87d1ea2a8665ab916f3e7a5c725c64
xsliang <xsliang@XSLIANG-PC>
2017-10-13 11:15:17 +0800

    test

--- Pic4chan/Program.cs
+++ Pic4chan/Program.cs
@@ -14,6 +14,7 @@ namespace Pic4chan
         [STAThread]
         static void Main()
         {
+            ///
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormMain());
